4174ed575d2ae118836979818c534710eab93b55
CarlosDanielMaturano <carlosdanielmaturano@proton.me>
2023-11-25 12:16:37 -0300

    Adds a simple setup script

--- run.sh
+++ run.sh
@@ -1,3 +1,15 @@
 stow .
 
 ln -s ${HOME}/.config/zsh/.zshrc ${HOME}/.zshrc
+
+scripts_folder="./bin/scripts"
+scripts_files=`ls $scripts_folder`
+
+for file in $scripts_files
+do
+  full_path=`readlink -f ${scripts_folder}/${file}`
+  link_path="/bin/"$file
+  echo Linking $full_path to $link_path
+  ln -s $full_path $link_path
+done
+
