1bf2329ba70166db2fb465608f236ce5a68acdd5
CarlosDanielMaturano <carlosdanielmaturano@proton.me>
2024-03-22 17:53:14 -0300

    New way of sourcing scripts files

--- setup.sh
+++ setup.sh
@@ -1,14 +1,17 @@
 stow .
 
+echo "linking zsh conf"
 ln -s ${HOME}/.config/zsh/.zshrc ${HOME}/.zshrc
+echo ".scripts directory"
+ln -s ./scripts $HOME/.scripts
 
-scripts_folder="./bin/scripts"
-scripts_files=`ls $scripts_folder`
+scripts_dir="./scripts"
+scripts_file=`ls $scripts_dir`
 
-for file in $scripts_files
+for file in $scripts_file
 do
-  full_path=`readlink -f ${scripts_folder}/${file}`
-  link_path="/bin/"$file
+  full_path=`readlink -f ${scripts_dir}/${file}`
+  link_path=$HOME/.local/bin/
   echo Linking $full_path to $link_path
   ln -s $full_path $link_path
 done
